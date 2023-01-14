ad913b5ed83a6ceb259d3873f6abbe53d01cfa9f
SamnangGit <phengsamnanggit@gmail.com>
2023-01-15 02:50:15 +0700

    update some components

--- src/App.jsx
+++ src/App.jsx
@@ -8,14 +8,27 @@ import AssessmentQ from "./components/AssessmentQ";
 import CompanyDetail from "./components/CompanyDetail";
 import BookApointment from "./components/BookApointment";
 
+import { BrowserRouter, Routes } from "react-router-dom";
+
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="bg-black h-screen">
-      <Navbar />
-      <MainPage />
-    </div>
+    <>
+      <div className="bg-black h-screen">
+        <Navbar />
+        <MainPage />
+      </div>
+
+      <BrowserRouter>
+        <div>
+          <Route exact path="/" component={CompanyDashboard} />
+          <Route path="/assessment" component={AssessmentQ} />
+          <Route path="/company/:id" component={CompanyDetail} />
+          <Route path="/book" component={BookApointment} />
+        </div>
+      </BrowserRouter>
+    </>
   );
 }
 
